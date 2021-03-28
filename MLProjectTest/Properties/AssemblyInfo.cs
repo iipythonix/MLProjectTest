0c9ad218e2ee9231fb2662982ae969e077ee7311
iipythonix <79859739+iipythonix@users.noreply.github.com>
2021-03-28 17:23:38 -0400

    Add files via upload

---
+++ MLProjectTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+using System.Diagnostics;
+using System.Runtime.Versioning;
+using MelonLoader;
+using MLProjectTest;
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyTitle("MLProjectTest")]
+[assembly: AssemblyDescription("Python Phas test")]
+[assembly: AssemblyCompany(null)]
+[assembly: AssemblyProduct("MLProjectTest")]
+[assembly: AssemblyCopyright("Copyright © Python 2021")]
+[assembly: AssemblyTrademark(null)]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: MelonInfo(typeof(Test), "MLProjectTest", "1.0.0", "Python", "Download here when done")]
+[assembly: MelonGame("Kinetic Games", "Phasmophobia")]
---
+++ MLProjectTest/Test.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+
+namespace MLProjectTest
+{
+    public class Test
+    {
+        public static class BuildInfo
+        {
+            public const string Name = "MLProjectTest";
+            public const string Description = "ML Test";
+            public const string Author = "Python";
+            public const string Company = null;
+            public const string Version = "1.0.0";
+            public const string DownloadLink = null;
+        }
+
+        public class TestMod : MelonMod
+        {
+            public override void OnApplicationStart() // Runs after Game Initialization.
+            {
+                MelonLogger.Log("OnApplicationStart");
+            }
+
+            public override void OnLevelIsLoading() // Runs when a Scene is Loading or when a Loading Screen is Shown. Currently only runs if the Mod is used in BONEWORKS.
+            {
+                MelonLogger.Log("OnLevelIsLoading");
+            }
+
+            public override void OnLevelWasLoaded(int level) // Runs when a Scene has Loaded.
+            {
+                MelonLogger.Log("OnLevelWasLoaded: " + level.ToString());
+            }
+
+            public override void OnLevelWasInitialized(int level) // Runs when a Scene has Initialized.
+            {
+                MelonLogger.Log("OnLevelWasInitialized: " + level.ToString());
+            }
+
+            public override void OnUpdate() // Runs once per frame.
+            {
+                MelonLogger.Log("OnUpdate");
+            }
+
+            public override void OnFixedUpdate() // Can run multiple times per frame. Mostly used for Physics.
+            {
+                MelonLogger.Log("OnFixedUpdate");
+            }
+
+            public override void OnLateUpdate() // Runs once per frame after OnUpdate and OnFixedUpdate have finished.
+            {
+                MelonLogger.Log("OnLateUpdate");
+            }
+
+            public override void OnGUI() // Can run multiple times per frame. Mostly used for Unity's IMGUI.
+            {
+                MelonLogger.Log("OnGUI");
+            }
+
+            public override void OnApplicationQuit() // Runs when the Game is told to Close.
+            {
+                MelonLogger.Log("OnApplicationQuit");
+            }
+
+            public override void OnModSettingsApplied() // Runs when Mod Preferences get saved to UserData/modprefs.ini.
+            {
+                MelonLogger.Log("OnModSettingsApplied");
+            }
+
+            public override void VRChat_OnUiManagerInit() // Runs upon VRChat's UiManager Initialization. Only runs if the Mod is used in VRChat.
+            {
+                MelonLogger.Log("VRChat_OnUiManagerInit");
+            }
+        }
+    }
+}
